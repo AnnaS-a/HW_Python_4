941c4ad8f3829369d42b42a7acbdc7dfd82e3424
anna <slukin1@yandex.ru>
2022-10-08 01:57:59 +0700

    Task4

---
+++ Task4.py
@@ -0,0 +1,40 @@
+# Задача4: Задана натуральная степень k. Сформировать случайным образом
+#  список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример: - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+
+k = int(input('Введите натуральную степень k: '))
+def create_list(k):
+    list_coeff = []
+    for i in range(k + 1):
+        list_coeff.append(randint(0, 101))    
+    return list_coeff
+
+def create_str(sp):
+    list = sp[::-1]
+    wr = ''
+    if len(list) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(list)):
+            if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
+                wr += f'{list[i]}x^{len(list) - i - 1}'
+                if list [i + 1] != 0:
+                    wr += ' + '
+            elif i == len(list) - 2 and list[i] != 0:
+                wr += f'{list[i]}x'
+                if list[i + 1] != 0:
+                    wr += ' + '
+            elif i == len(list) - 1 and list[i] != 0:
+                wr += f'{list[i]} = 0'
+            elif i == len(list) - 1 and list[i] == 0:
+                wr += ' = 0'
+    return wr
+
+def write_file(st):
+    with open('task4_2.txt', 'w') as data:
+        data.write(st)
+
+koef = create_list(k)
+write_file(create_str(koef))
